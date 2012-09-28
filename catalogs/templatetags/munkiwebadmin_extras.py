3e34bc59a4cf4b57af1a0559ddba164ae5d07d16
Adam Reed <adam.reed@anu.edu.au>
2012-09-28 14:57:41 +1000

    Improve handling of long name and version strings in the catalog table. Two approaches are used, the first is by adding hints before underscores and full stops to tell the browser that it may wrap the text at that point. The second is to use the css word-wrap property to forcibly wrap text. This has required that the version column was given a set width. To ensure there is no confusion if version numbers are wrapped, a horizontal line is added between versions if multiple

---
+++ catalogs/templatetags/munkiwebadmin_extras.py
@@ -0,0 +1,32 @@
+from django import template
+from django.template.defaultfilters import stringfilter
+from django.utils.html import conditional_escape
+from django.utils.safestring import mark_safe
+
+# Reference for creating custom filters and tags:-
+# https://docs.djangoproject.com/en/dev/howto/custom-template-tags/
+
+register = template.Library()
+
+@register.filter(name='wrappable', needs_autoescape=True)
+@stringfilter
+def wrappable_filter(value, autoescape=None):
+    """
+    Places zero-width breaking spaces in text to allow it to wrap 
+    before a underscore (_) or fullstop (.). The browser will use this
+    as a hint as to where to wrap the text if required.
+    """
+    
+    # Escape the input text so that it's safe to display
+    if autoescape:
+        esc = conditional_escape
+    else:
+        esc = lambda x: x
+    escaped_wrapped_text = '%s' % (esc(value))
+
+    # Add hints to the browser that it can wrap before a _ of .
+    escaped_wrapped_text = escaped_wrapped_text.replace('_', '&#8203;_')
+    escaped_wrapped_text = escaped_wrapped_text.replace('.', '&#8203;.')
+
+    # Return the text marked as safe so that the new &#8203;'s aren't escaped
+    return mark_safe(escaped_wrapped_text)
