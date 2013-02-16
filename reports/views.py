munki/munkiwebadmin

commit 3e7f367df4041881e59ff4d5367a5c9994976e99

Minor tweaks to reports/views.py for display

reports/views.py
@@ -58,8 +58,10 @@ def submit(request, submission_type):
             # extract machine data from the report
             report_data = report.get_report()
             if 'MachineInfo' in report_data:
-                machine.os_version = report_data['MachineInfo'].get('os_vers')
-                machine.cpu_arch = report_data['MachineInfo'].get('arch')
+                machine.os_version = report_data['MachineInfo'].get(
+                    'os_vers', 'UNKNOWN')
+                machine.cpu_arch = report_data['MachineInfo'].get(
+                    'arch', 'UNKNOWN')
             machine.available_disk_space = \
                 report_data.get('AvailableDiskSpace') or 0
             hwinfo = {}
@@ -312,7 +314,7 @@ def formatted_manafactured_date(year, week):
         ret -= timedelta(days=7)
 
     # Format Day
-    day = ret.strftime('%d')
+    day = int(ret.strftime('%d'))
     if day == 1 or day == 11 or day == 21 or day == 31:
         suffix = "st"
     elif day == 2 or day == 12 or day == 22:

commit 11f2bfb0c60d2c23ea8f9b9a281380f3dd093c7b

Fix for 'int' object has no attribute 'lstrip' error on the machine reports detail page introduced in commit 3e7f367df404.

reports/views.py
@@ -314,7 +314,7 @@ def formatted_manafactured_date(year, week):
         ret -= timedelta(days=7)
 
     # Format Day
-    day = int(ret.strftime('%d'))
+    day = ret.strftime('%d')
     if day == 1 or day == 11 or day == 21 or day == 31:
         suffix = "st"
     elif day == 2 or day == 12 or day == 22: